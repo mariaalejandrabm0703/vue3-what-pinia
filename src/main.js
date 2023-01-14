43bfec257df4678311e41a0c376bc6360ab9c69a
Maria Bohorquez <maria.bohorquez@ceiba.com.co>
2023-01-13 21:07:30 -0500

    example pinia async

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+//Import Pinia into your config file
+import { createPinia } from "pinia";
+
+createApp(App)
+  //Add the line below to the file to instantiate it
+  .use(createPinia())
+  .mount("#app");
